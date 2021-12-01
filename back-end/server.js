575b0dedf72cd1d047d38472a4b30ad7f41eeee7
Paulin Antoine <antoine_paulin@yahoo.com>
2021-12-01 11:59:19 +0100

    Modification du fichier server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,6 @@
 //Lancement du serveur sur le localhost:3000.
 
-const http = require('http');
+/*const http = require('http');
 const app = require('./app');
 const normalizePort = val => {
     const port = parseInt(val, 10);
@@ -16,4 +16,52 @@ const normalizePort = val => {
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 const server = http.createServer(app);
+server.listen(port); */
+
+const http = require('http');
+const app = require('./app');
+
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
 server.listen(port);
